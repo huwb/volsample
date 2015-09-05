9f01d9917e4c9210372b7dd512b37a88c55470ce
huwb <huw.bowles@gmail.com>
2015-09-06 00:59:02 +0200

    Experimental stuff

---
+++ src/unity/Assets/Scripts/Experimental/DDASampling.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class DDASampling : MonoBehaviour
+{
+	public int samplesPerSliceDir = 20;
+	public float rayCount = 20f;
+	public float fovMult = 0.2f;
+	public bool l1Sampling = false;
+
+	void Start () {
+	
+	}
+	
+	void Update()
+	{
+		for( float i = 0f; i < rayCount; i += 1f )
+		{
+			float x = (i-rayCount/2f)*fovMult;
+			Vector3 rd = transform.forward + x * transform.right;
+			SampleRay( transform.position, rd );
+		}
+		//DrawSample( transform.position + 3f * transform.forward );
+	}
+
+	void SampleRay( Vector3 ro, Vector3 rd )
+	{
+		List< Vector3 > samples = new List<Vector3>();
+
+		SampleSlices( ro, rd, Vector3.right, samples );
+		SampleSlices( ro, rd, Vector3.forward, samples );
+
+		foreach( Vector3 s in samples )
+			DrawSample( s );
+	}
+
+	void SampleSlices( Vector3 ro, Vector3 rd, Vector3 n, List< Vector3 > samples )
+	{
+		float dpDir = Vector3.Dot( rd, n );
+		if( Mathf.Abs( dpDir ) > Mathf.Epsilon )
+		{
+			float dpOri = Vector3.Dot( ro, n );
+			float distPerX = Mathf.Abs( 1.0f / dpDir );
+			float firstIntX = Mathf.Ceil( dpOri - 1f );
+			Vector3 roX = ro + (dpOri-firstIntX) * distPerX * rd;
+			float t = 1f * distPerX;
+			for( int i = 0; i < samplesPerSliceDir; i++ )
+			{
+				bool takeIt = !l1Sampling || Mathf.Abs(dpDir) > 1f/Mathf.Sqrt(2f);
+
+				if( takeIt )
+					samples.Add( roX + rd * t );
+
+				t += distPerX;
+			}
+		}
+	}
+	void DrawSample( Vector3 pos )
+	{
+		float s = 0.2f;
+		Debug.DrawLine( pos - s*Vector3.forward, pos + s*Vector3.forward );
+		Debug.DrawLine( pos - s*Vector3.right, pos + s*Vector3.right );
+		Debug.DrawLine( pos - s*Vector3.up, pos + s*Vector3.up );
+	}
+}
---
+++ src/unity/Assets/Scripts/Experimental/L0_Isolines.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class L0_Isolines : MonoBehaviour
+{
+	void Start () {
+	
+	}
+
+	float weightKernel( float x, float z )
+	{
+		return 1.0f - Mathf.Max( Mathf.Abs(x), Mathf.Abs(z) );
+	}
+
+	void Update()
+	{
+		float posX = transform.position.x;
+		float fracX = Mathf.Repeat( posX, 1f );
+		posX -= fracX;
+		float posZ = transform.position.z;
+		float fracZ = Mathf.Repeat( posZ, 1f );
+		posZ -= fracZ;
+
+		DrawStructure( weightKernel( -fracX, -fracZ ), posX, posZ );
+		DrawStructure( weightKernel( 1f-fracX, -fracZ ), posX+1f, posZ );
+		DrawStructure( weightKernel( -fracX, 1f-fracZ ), posX, posZ+1f );
+		DrawStructure( weightKernel( 1f-fracX, 1f-fracZ ), posX+1f, posZ+1f );
+
+		/*
+		float cx = posX + 0.5f;
+		float cz = posZ + 0.5f;
+		for( float i = 0f; i < 10f; i += 1f )
+		{
+			float r = i+0.5f;
+
+			// top x
+			Debug.DrawLine( new Vector3( cx-r-1f, 0f, cz+r ), new Vector3( cx-r, 0f, cz+r ), new Color(1f,0f,0f,(1f-fracZ)*(1f-fracX)) );
+			Debug.DrawLine( new Vector3( cx+r-1f, 0f, cz+r ), new Vector3( cx+r, 0f, cz+r ), new Color(1f,0f,0f,(1f-fracZ)*(1f-fracX)) );
+			Debug.DrawLine( new Vector3( cx-r, 0f, cz+r ), new Vector3( cx-r+1f, 0f, cz+r ), new Color(1f,0f,0f,(1f-fracZ)*fracX) );
+			Debug.DrawLine( new Vector3( cx+r+1f, 0f, cz+r ), new Vector3( cx+r, 0f, cz+r ), new Color(1f,0f,0f,(1f-fracZ)*fracX) );
+			// bottom x
+			Debug.DrawLine( new Vector3( cx-r-1f, 0f, cz-r ), new Vector3( cx-r, 0f, cz-r ), new Color(1f,0f,0f,(fracZ)*(1f-fracX)) );
+			Debug.DrawLine( new Vector3( cx+r-1f, 0f, cz-r ), new Vector3( cx+r, 0f, cz-r ), new Color(1f,0f,0f,(fracZ)*(1f-fracX)) );
+			Debug.DrawLine( new Vector3( cx-r, 0f, cz-r ), new Vector3( cx-r+1f, 0f, cz-r ), new Color(1f,0f,0f,(fracZ)*fracX) );
+			Debug.DrawLine( new Vector3( cx+r+1f, 0f, cz-r ), new Vector3( cx+r, 0f, cz-r ), new Color(1f,0f,0f,(fracZ)*fracX) );
+			// right z
+			Debug.DrawLine( new Vector3( cx+r, 0f, cz+r+1f ), new Vector3( cx+r, 0f, cz+r ), new Color(1f,0f,0f,fracZ*(1f-fracX)) );
+			Debug.DrawLine( new Vector3( cx+r, 0f, cz-r-1f ), new Vector3( cx+r, 0f, cz-r ), new Color(1f,0f,0f,(1f-fracZ)*(1f-fracX)) );
+			Debug.DrawLine( new Vector3( cx+r, 0f, cz-r ), new Vector3( cx+r, 0f, cz-r+1f ), new Color(1f,0f,0f,(1f-fracZ)*fracX) );
+			Debug.DrawLine( new Vector3( cx+r, 0f, cz+r+1f ), new Vector3( cx+r, 0f, cz+r ), new Color(1f,0f,0f,fracZ*(1f-fracX)) );
+			// left z
+			Debug.DrawLine( new Vector3( cx-r, 0f, cz+r+1f ), new Vector3( cx-r, 0f, cz+r ), new Color(1f,0f,0f,fracZ*(fracX)) );
+			Debug.DrawLine( new Vector3( cx-r, 0f, cz-r-1f ), new Vector3( cx-r, 0f, cz-r ), new Color(1f,0f,0f,(1f-fracZ)*(fracX)) );
+			Debug.DrawLine( new Vector3( cx-r, 0f, cz+r+1f ), new Vector3( cx-r, 0f, cz+r ), new Color(1f,0f,0f,fracZ*(fracX)) );
+			Debug.DrawLine( new Vector3( cx-r, 0f, cz-r ), new Vector3( cx-r, 0f, cz-r+1f ), new Color(1f,0f,0f,(fracZ)*(fracX)) );
+		}
+		*/
+	}
+
+	void DrawStructure(float alpha, float posX, float posZ )
+	{
+		for( float i = 1f; i < 10f; i++ )
+		{
+			//Debug.DrawLine( new Vector3( posX-i, 0f, posZ+i ), new Vector3( posX-(i-1f), 0f, posZ+i ), new Color(1f,0f,0f,alpha) );
+			Debug.DrawLine( new Vector3( posX-i, 0f, posZ+i ), new Vector3( posX+i, 0f, posZ+i ), new Color(1f,1f,1f,alpha) );
+
+			Debug.DrawLine( new Vector3( posX-i, 0f, posZ-i ), new Vector3( posX+i, 0f, posZ-i ), new Color(1f,1f,1f,alpha) );
+			Debug.DrawLine( new Vector3( posX+i, 0f, posZ-i ), new Vector3( posX+i, 0f, posZ+i ), new Color(1f,1f,1f,alpha) );
+			Debug.DrawLine( new Vector3( posX-i, 0f, posZ-i ), new Vector3( posX-i, 0f, posZ+i ), new Color(1f,1f,1f,alpha) );
+		}
+	}
+}
