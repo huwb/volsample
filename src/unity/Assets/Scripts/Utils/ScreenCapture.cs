6baf25c1444f1aa966afe8ce2d0412bf176b9758
huwb <huw.bowles@gmail.com>
2016-08-27 19:00:03 +0100

    Config for capture. Extended screen capture script. Max time step is 1/50.

--- src/unity/Assets/Scripts/Utils/ScreenCapture.cs
+++ src/unity/Assets/Scripts/Utils/ScreenCapture.cs
@@ -4,14 +4,39 @@ public class ScreenCapture : MonoBehaviour
 {
 	public bool on = false;
 
+    public int _maxScreenshots = -1;
+
 	int screenShotNumber = 0;
+
+    int _lastCapturedFrameCount = -1;
+
+    string _folderName = "C:/Unity-Capture";
+
+    void Start()
+    {
+        if( !System.IO.Directory.Exists( _folderName ) )
+        {
+            System.IO.Directory.CreateDirectory( _folderName );
+            Debug.Log( "Created screenshot folder: " + _folderName );
+        }
+    }
 
 	void OnPostRender()
 	{
-		if (on)
+		if( on && Time.frameCount != _lastCapturedFrameCount )
 		{
-			Application.CaptureScreenshot ("C:/Unity-Capture/Screenshot" + screenShotNumber.ToString ("D3") + ".png");
+            _lastCapturedFrameCount = Time.frameCount;
+
+            string screenName = _folderName + "/Screenshot" + screenShotNumber.ToString( "D5" ) + ".png";
+            Application.CaptureScreenshot( screenName );
+            Debug.Log( "Saved screenshot: " + screenName );
+
 			++screenShotNumber;
+
+            if( screenShotNumber == _maxScreenshots )
+            {
+                on = false;
+            }
 		}
 	}
 }
