e03d90bc41b4b9e927a6cab601df68be4269d712
huwb <huw.bowles@gmail.com>
2017-01-10 20:19:06 +0000

    Update to Unity 5.5

--- src/unity/Assets/Standard Assets/Effects/ImageEffects/Scripts/PostEffectsBase.cs
+++ src/unity/Assets/Standard Assets/Effects/ImageEffects/Scripts/PostEffectsBase.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace UnityStandardAssets.ImageEffects
@@ -94,7 +93,7 @@ namespace UnityStandardAssets.ImageEffects
             supportHDRTextures = SystemInfo.SupportsRenderTextureFormat(RenderTextureFormat.ARGBHalf);
             supportDX11 = SystemInfo.graphicsShaderLevel >= 50 && SystemInfo.supportsComputeShaders;
 
-            if (!SystemInfo.supportsImageEffects || !SystemInfo.supportsRenderTextures)
+            if (!SystemInfo.supportsImageEffects )
 			{
                 NotSupported ();
                 return false;
@@ -159,85 +158,6 @@ namespace UnityStandardAssets.ImageEffects
             enabled = false;
             isSupported = false;
             return;
-        }
-
-
-        protected void DrawBorder (RenderTexture dest, Material material)
-		{
-            float x1;
-            float x2;
-            float y1;
-            float y2;
-
-            RenderTexture.active = dest;
-            bool  invertY = true; // source.texelSize.y < 0.0ff;
-            // Set up the simple Matrix
-            GL.PushMatrix();
-            GL.LoadOrtho();
-
-            for (int i = 0; i < material.passCount; i++)
-            {
-                material.SetPass(i);
-
-                float y1_; float y2_;
-                if (invertY)
-                {
-                    y1_ = 1.0f; y2_ = 0.0f;
-                }
-                else
-                {
-                    y1_ = 0.0f; y2_ = 1.0f;
-                }
-
-                // left
-                x1 = 0.0f;
-                x2 = 0.0f + 1.0f/(dest.width*1.0f);
-                y1 = 0.0f;
-                y2 = 1.0f;
-                GL.Begin(GL.QUADS);
-
-                GL.TexCoord2(0.0f, y1_); GL.Vertex3(x1, y1, 0.1f);
-                GL.TexCoord2(1.0f, y1_); GL.Vertex3(x2, y1, 0.1f);
-                GL.TexCoord2(1.0f, y2_); GL.Vertex3(x2, y2, 0.1f);
-                GL.TexCoord2(0.0f, y2_); GL.Vertex3(x1, y2, 0.1f);
-
-                // right
-                x1 = 1.0f - 1.0f/(dest.width*1.0f);
-                x2 = 1.0f;
-                y1 = 0.0f;
-                y2 = 1.0f;
-
-                GL.TexCoord2(0.0f, y1_); GL.Vertex3(x1, y1, 0.1f);
-                GL.TexCoord2(1.0f, y1_); GL.Vertex3(x2, y1, 0.1f);
-                GL.TexCoord2(1.0f, y2_); GL.Vertex3(x2, y2, 0.1f);
-                GL.TexCoord2(0.0f, y2_); GL.Vertex3(x1, y2, 0.1f);
-
-                // top
-                x1 = 0.0f;
-                x2 = 1.0f;
-                y1 = 0.0f;
-                y2 = 0.0f + 1.0f/(dest.height*1.0f);
-
-                GL.TexCoord2(0.0f, y1_); GL.Vertex3(x1, y1, 0.1f);
-                GL.TexCoord2(1.0f, y1_); GL.Vertex3(x2, y1, 0.1f);
-                GL.TexCoord2(1.0f, y2_); GL.Vertex3(x2, y2, 0.1f);
-                GL.TexCoord2(0.0f, y2_); GL.Vertex3(x1, y2, 0.1f);
-
-                // bottom
-                x1 = 0.0f;
-                x2 = 1.0f;
-                y1 = 1.0f - 1.0f/(dest.height*1.0f);
-                y2 = 1.0f;
-
-                GL.TexCoord2(0.0f, y1_); GL.Vertex3(x1, y1, 0.1f);
-                GL.TexCoord2(1.0f, y1_); GL.Vertex3(x2, y1, 0.1f);
-                GL.TexCoord2(1.0f, y2_); GL.Vertex3(x2, y2, 0.1f);
-                GL.TexCoord2(0.0f, y2_); GL.Vertex3(x1, y2, 0.1f);
-
-                GL.End();
-            }
-
-            GL.PopMatrix();
         }
     }
 }
