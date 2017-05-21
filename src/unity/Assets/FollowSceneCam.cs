640bd4090f6f162b42ccc61ae8ff4748296577f9
huwb <huw.bowles@gmail.com>
2017-05-21 11:45:00 +0100

    Utility script to snap a camera to the scene view camera, for convenience in edit mode

---
+++ src/unity/Assets/FollowSceneCam.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class FollowSceneCam : MonoBehaviour
+{
+    public bool _apply = false;
+	
+	void OnDrawGizmos()
+    {
+        if( _apply )
+        {
+            Camera sceneViewCam = Camera.current;
+            if( sceneViewCam != null )
+            {
+                transform.position = sceneViewCam.transform.position;
+                transform.rotation = sceneViewCam.transform.rotation;
+                //transform.localScale = sceneViewCam.transform.localScale;
+            }
+        }
+    }
+}
